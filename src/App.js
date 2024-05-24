5aedd002e933793a5fbcd848da9ebe61cc564e61
ekkaterinasmirnovaa <ekkaterinasmirnovaa@gmail.com>
2024-05-24 19:04:08 +0300

    draft v2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import "./App.css";
+import katarium from "./images/Katarium_start.png";
+import waterfall from "./images/waterfall.png";
 
-function CollapseButton({ name, children }) {
+function CollapseButton({ name, children, symbol }) {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
@@ -19,13 +21,60 @@ function CollapseButton({ name, children }) {
             marginTop: "5px",
           }}
         >
-          {">"}
+          {symbol}
         </div>
       </button>
       {isOpen && children}
     </div>
   );
 }
+
+function CollapseButtonv2({ name, children, symbol }) {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+        className="sign-in"
+      >
+        {name}{" "}
+        <div
+          style={{
+            transform: isOpen ? "rotate(-90deg)" : "rotate(90deg)",
+            marginTop: "5px",
+          }}
+        >
+          {symbol}
+        </div>
+      </button>
+      {isOpen && children}
+    </div>
+  );
+}
+
+// function InfoPage() {
+//   return <>
+//     <button>{"123"}</button>
+//     <div style={{ flexGrow: 1 }}>
+//       <h1>
+//         Информационно учебный полигон "Катариум"
+//       </h1>
+//       <span>
+//         О системе
+//       </span>
+//       <p>
+//         Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem ipsum
+//         v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+//         v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+//         v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+//         v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+//         v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+//       </p>
+//     </div>
+//   </>
+// }
 
 function Burger({ isOpen, open, close }) {
   return (
@@ -39,43 +88,116 @@ function Burger({ isOpen, open, close }) {
           }
         }}
       >
-        {isOpen ? "X" : "__"}
+        {isOpen ? "111" : "__"}
       </button>
     </div>
   );
 }
 
+function Info({ isOpen, open, close }) {
+  return <div>
+    <button
+      onClick={() => {
+        if (isOpen) {
+          close();
+        } else {
+          open();
+        }
+      }}
+    >
+      {isOpen ? ">" : "<"}
+    </button>
+  </div>
+}
+
 function AuthPage() {
+  const [infoIsOpen, setInfoIsOpen] = useState(false);
   return (
     <>
       <div style={{ flexGrow: 1 }}>
-        <CollapseButton name="Вход">
+        <CollapseButton name="Вход" symbol={">"}>
           <div>Вход</div>
         </CollapseButton>
-        <CollapseButton name="Регистрация">
+        <CollapseButton name="Регистрация" symbol={">"}>
           <div>Регистрация</div>
         </CollapseButton>
       </div>
-      <button>{">"}</button>
+      <button>{"XXX"}</button>
+      {/* <button>{"123"}</button> */}
+      {/* <button
+        onClick={() => }
+      >
+      </button> */}
+      {/* <button symbol={"v"} ><div>
+        <h1>
+          Информационно учебный полигон "Катариум"
+        </h1>
+        <span>
+          О системе
+        </span>
+        <p>
+          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem ipsum
+          v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+          v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+          v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+          v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+          v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+        </p>
+      </div></button> */}
     </>
   );
 }
 
+
+
 function LandingPage({ goToAuth }) {
   return (
     <div>
-      Landing page <button onClick={goToAuth}>Auth</button>
+
+      {/* <button onClick={goToAuth}>Auth</button> */}
+      {/* Landing page */}
+      <img src={katarium} alt="katarium" style={{ height: "145vh", }} />
+      <h1>ИСТОРИЯ</h1>
+      <p>В 1550-х годах на севере острова Итугум, омываемом
+        Аравийским морем, сложилось господство королевства
+        Катарий, претендующего на обладание абсолютной
+        властью. Оно постоянно вело разрушительные
+        захватнические войны с соседями, завоевывая
+        и присоединяя к себе все новые территории.
+      </p>
+      <p>
+        На южной стороне острова располагалось небольшое,
+        но сильное королевство Риум. Его земли располагались
+        вдоль крупных полноводных рек Гицем и Сея, а также в
+        Ямчинских горах. Его населяли воинственные племена, грозно защищавшие границы своего королевства и
+        совершающие стремительные набеги на
+        территории Катария, доставляя много хлопот его властям.
+      </p>
+      <img src={waterfall} alt="waterfall"></img>
     </div>
   );
 }
 
-function BurgerPage() {
-  return <div>Burger page</div>;
+// function BurgerPage() {
+//   return <div>Burger page</div>;
+// }
+
+function InfoPage() {
+  return <div><h1>Информационно учебный полигон Катариум</h1>
+    <p>О системе</p>
+    <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem ipsum
+      v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+      v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+      v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+      v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+      v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+    </p></div>
 }
 
 function App() {
   const [page, setPage] = useState("auth");
   const [burgerIsOpen, setBurgerIsOpen] = useState(false);
+  const [infoIsOpen, setInfoIsOpen] = useState(false);
   return (
     <div style={{ height: "100vh", display: "flex", flexDirection: "column" }}>
       {page === "landing" ? (
@@ -120,9 +242,39 @@ function App() {
               </div>
             ) : page === "auth" ? (
               <AuthPage />
-            ) : page === "burger" ? (
-              <BurgerPage />
-            ) : null}
+              // ) : page === "burger" ? (
+              //   <BurgerPage />
+              // ) : page === "info" ? (
+              //   <InfoPage />
+            )
+              : infoIsOpen ? (
+                <div style={{ border: "1px solid black", width: "100%" }}><button
+                  onClick={() => {
+                    setPage("landing");
+                    setInfoIsOpen(false);
+                  }}
+                  style={{ display: "block", marginTop: "10px" }}
+                >
+                  1234567
+                  {/* <h1>Информационно учебный полигон Катариум</h1>
+                  <p>О системе</p>
+                  <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem ipsum
+                    v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+                    v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+                    v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+                    v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+                    v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+                  </p> */}
+                </button>
+                </div>
+              ) : page === "auth" ? (
+                <InfoPage />
+                // ) : page === "burger" ? (
+                //   <BurgerPage />
+                // ) : page === "info" ? (
+                //   <InfoPage />
+              )
+                : null}
           </main>
         </>
       )}
