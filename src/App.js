4300187b7b0613359af341f43eb8c80ff0eeb750
ekkaterinasmirnovaa <ekkaterinasmirnovaa@gmail.com>
2024-05-28 00:35:44 +0300

    v4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import "./LoginForm.css";
 import katarium from "./images/Katarium_start.png";
 import waterfall from "./images/waterfall.png";
 
@@ -12,7 +13,7 @@ function CollapseButton({ name, children, symbol }) {
           setIsOpen(!isOpen);
         }}
         className="sign-in"
-        style={{ display: "flex", gap: "10px", width: "100%" }}
+        style={{ display: "flex", gap: "10px", width: "100%", fontfontFamily: "Futura, sans serif", borderBottom: "0px solid #000000", borderRight: "0px" }}
       >
         {name}{" "}
         <div
@@ -25,44 +26,44 @@ function CollapseButton({ name, children, symbol }) {
         </div>
       </button>
       {isOpen && children}
-    </div>
+    </div >
   );
 }
 
-function CollapseButtonv2({ name, children, symbol }) {
-  const [isOpen, setIsOpen] = useState(false);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setIsOpen(!isOpen);
-        }}
-        className="sign-in"
-      >
-        {name}{" "}
-        <div
-          style={{
-            transform: isOpen ? "rotate(-90deg)" : "rotate(90deg)",
-            marginTop: "5px",
-          }}
-        >
-          {symbol}
-        </div>
-      </button>
-      {isOpen && children}
-    </div>
-  );
-}
+// function CollapseButtonv2({ name, children, symbol }) {
+//   const [isOpen, setIsOpen] = useState(false);
+//   return (
+//     <div>
+//       <button
+//         onClick={() => {
+//           setIsOpen(!isOpen);
+//         }}
+//         className="sign-in"
+//       >
+//         {name}{" "}
+//         <div
+//           style={{
+//             transform: isOpen ? "rotate(-90deg)" : "rotate(90deg)",
+//             marginTop: "5px",
+//           }}
+//         >
+//           {symbol}
+//         </div>
+//       </button>
+//       {isOpen && children}
+//     </div>
+//   );
+// }
 
 // function InfoPage() {
 //   return <>
 //     <button>{"123"}</button>
 //     <div style={{ flexGrow: 1 }}>
 //       <h1>
-//         Информационно учебный полигон "Катариум"
+//         РРЅС„РѕСЂРјР°С†РёРѕРЅРЅРѕ СѓС‡РµР±РЅС‹Р№ РїРѕР»РёРіРѕРЅ "РљР°С‚Р°СЂРёСѓРј"
 //       </h1>
 //       <span>
-//         О системе
+//         Рћ СЃРёСЃС‚РµРјРµ
 //       </span>
 //       <p>
 //         Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem ipsum
@@ -95,34 +96,67 @@ function Burger({ isOpen, open, close }) {
 }
 
 function Info({ isOpen, open, close }) {
-  return <div>
-    <button
-      onClick={() => {
-        if (isOpen) {
-          close();
-        } else {
-          open();
-        }
-      }}
-    >
-      {isOpen ? ">" : "<"}
-    </button>
-  </div>
+  return (
+    <div>
+      <button
+        onClick={() => {
+          if (isOpen) {
+            close();
+          } else {
+            open();
+          }
+        }}
+      >
+        {isOpen ? ">" : "<"}
+      </button>
+    </div>
+  );
 }
 
-function AuthPage() {
+
+function LoginForm() {
+  return (
+    <div className="ring">
+      {/* <i style={{ '--clr': '#00ff0a' }}></i>
+      <i style={{ '--clr': '#ff0057' }}></i>
+      <i style={{ '--clr': '#fffd44' }}></i> */}
+      <div className="login">
+        {/* <h2>Login</h2> */}
+        <div className="inputBx">
+          <input type="text" placeholder="Логин" />
+        </div>
+        <div className="inputBx">
+          <input type="password" placeholder="Пароль" />
+        </div>
+        <div className="inputBx">
+          <input type="submit" value="Войти" />
+        </div>
+        <div className="links">
+          <a href="#">Восстановление доступа</a>
+          {/* <a href="#">Signup</a> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+
+
+
+function AuthPage({ goToAutismPage }) {
   const [infoIsOpen, setInfoIsOpen] = useState(false);
   return (
     <>
-      <div style={{ flexGrow: 1 }}>
+      <div style={{ flexGrow: 1, background: "#EFEEEC" }}>
         <CollapseButton name="Вход" symbol={">"}>
-          <div>Вход</div>
+          <div><LoginForm /></div>
         </CollapseButton>
         <CollapseButton name="Регистрация" symbol={">"}>
-          <div>Регистрация</div>
+          <div>Р РµРіРёСЃС‚СЂР°С†РёСЏ</div>
         </CollapseButton>
       </div>
-      <button>{"XXX"}</button>
+      <button onClick={goToAutismPage}>{"XXX"}</button>
       {/* <button>{"123"}</button> */}
       {/* <button
         onClick={() => }
@@ -130,10 +164,10 @@ function AuthPage() {
       </button> */}
       {/* <button symbol={"v"} ><div>
         <h1>
-          Информационно учебный полигон "Катариум"
+          РРЅС„РѕСЂРјР°С†РёРѕРЅРЅРѕ СѓС‡РµР±РЅС‹Р№ РїРѕР»РёРіРѕРЅ "РљР°С‚Р°СЂРёСѓРј"
         </h1>
         <span>
-          О системе
+          Рћ СЃРёСЃС‚РµРјРµ
         </span>
         <p>
           Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem ipsum
@@ -148,30 +182,25 @@ function AuthPage() {
   );
 }
 
-
-
 function LandingPage({ goToAuth }) {
   return (
     <div>
-
       {/* <button onClick={goToAuth}>Auth</button> */}
       {/* Landing page */}
-      <img src={katarium} alt="katarium" style={{ height: "145vh", }} />
-      <h1>ИСТОРИЯ</h1>
-      <p>В 1550-х годах на севере острова Итугум, омываемом
-        Аравийским морем, сложилось господство королевства
-        Катарий, претендующего на обладание абсолютной
-        властью. Оно постоянно вело разрушительные
-        захватнические войны с соседями, завоевывая
-        и присоединяя к себе все новые территории.
+      <img src={katarium} alt="katarium" style={{ height: "145vh" }} />
+      <h1>РРЎРўРћР РРЇ</h1>
+      <p>
+        Р’ 1550-С… РіРѕРґР°С… РЅР° СЃРµРІРµСЂРµ РѕСЃС‚СЂРѕРІР° РС‚СѓРіСѓРј, РѕРјС‹РІР°РµРјРѕРј РђСЂР°РІРёР№СЃРєРёРј РјРѕСЂРµРј,
+        СЃР»РѕР¶РёР»РѕСЃСЊ РіРѕСЃРїРѕРґСЃС‚РІРѕ РєРѕСЂРѕР»РµРІСЃС‚РІР° РљР°С‚Р°СЂРёР№, РїСЂРµС‚РµРЅРґСѓСЋС‰РµРіРѕ РЅР° РѕР±Р»Р°РґР°РЅРёРµ
+        Р°Р±СЃРѕР»СЋС‚РЅРѕР№ РІР»Р°СЃС‚СЊСЋ. РћРЅРѕ РїРѕСЃС‚РѕСЏРЅРЅРѕ РІРµР»Рѕ СЂР°Р·СЂСѓС€РёС‚РµР»СЊРЅС‹Рµ Р·Р°С…РІР°С‚РЅРёС‡РµСЃРєРёРµ
+        РІРѕР№РЅС‹ СЃ СЃРѕСЃРµРґСЏРјРё, Р·Р°РІРѕРµРІС‹РІР°СЏ РёВ РїСЂРёСЃРѕРµРґРёРЅСЏСЏ Рє СЃРµР±Рµ РІСЃРµ РЅРѕРІС‹Рµ С‚РµСЂСЂРёС‚РѕСЂРёРё.
       </p>
       <p>
-        На южной стороне острова располагалось небольшое,
-        но сильное королевство Риум. Его земли располагались
-        вдоль крупных полноводных рек Гицем и Сея, а также в
-        Ямчинских горах. Его населяли воинственные племена, грозно защищавшие границы своего королевства и
-        совершающие стремительные набеги на
-        территории Катария, доставляя много хлопот его властям.
+        РќР° СЋР¶РЅРѕР№ СЃС‚РѕСЂРѕРЅРµ РѕСЃС‚СЂРѕРІР° СЂР°СЃРїРѕР»Р°РіР°Р»РѕСЃСЊ РЅРµР±РѕР»СЊС€РѕРµ, РЅРѕВ СЃРёР»СЊРЅРѕРµ РєРѕСЂРѕР»РµРІСЃС‚РІРѕ
+        Р РёСѓРј. Р•РіРѕ Р·РµРјР»Рё СЂР°СЃРїРѕР»Р°РіР°Р»РёСЃСЊ РІРґРѕР»СЊ РєСЂСѓРїРЅС‹С… РїРѕР»РЅРѕРІРѕРґРЅС‹С… СЂРµРє Р“РёС†РµРј Рё РЎРµСЏ,
+        Р° С‚Р°РєР¶Рµ РІ РЇРјС‡РёРЅСЃРєРёС… РіРѕСЂР°С…. Р•РіРѕ РЅР°СЃРµР»СЏР»Рё РІРѕРёРЅСЃС‚РІРµРЅРЅС‹Рµ РїР»РµРјРµРЅР°, РіСЂРѕР·РЅРѕ
+        Р·Р°С‰РёС‰Р°РІС€РёРµ РіСЂР°РЅРёС†С‹ СЃРІРѕРµРіРѕ РєРѕСЂРѕР»РµРІСЃС‚РІР° Рё СЃРѕРІРµСЂС€Р°СЋС‰РёРµ СЃС‚СЂРµРјРёС‚РµР»СЊРЅС‹Рµ РЅР°Р±РµРіРё
+        РЅР° С‚РµСЂСЂРёС‚РѕСЂРёРё РљР°С‚Р°СЂРёСЏ, РґРѕСЃС‚Р°РІР»СЏСЏ РјРЅРѕРіРѕ С…Р»РѕРїРѕС‚ РµРіРѕ РІР»Р°СЃС‚СЏРј.
       </p>
       <img src={waterfall} alt="waterfall"></img>
     </div>
@@ -183,15 +212,35 @@ function LandingPage({ goToAuth }) {
 // }
 
 function InfoPage() {
-  return <div><h1>Информационно учебный полигон Катариум</h1>
-    <p>О системе</p>
-    <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem ipsum
-      v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
-      v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
-      v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
-      v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
-      v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
-    </p></div>
+  return (
+    <div>
+      <h1>РРЅС„РѕСЂРјР°С†РёРѕРЅРЅРѕ СѓС‡РµР±РЅС‹Р№ РїРѕР»РёРіРѕРЅ РљР°С‚Р°СЂРёСѓРј</h1>
+      <p>Рћ СЃРёСЃС‚РµРјРµ</p>
+      <p>
+        Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsum
+        Lorem ipsumLorem ipsum v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+        Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
+        ipsumLorem ipsum v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+        ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
+        ipsumLorem ipsum v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+        ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
+        ipsumLorem ipsum v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+        ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
+        ipsumLorem ipsum v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+        ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
+        ipsumLorem ipsum
+      </p>
+    </div>
+  );
+}
+
+function AutismPage({ goToAuth }) {
+  return (
+    <div style={{ display: "flex", background: "purple" }}>
+      <button onClick={goToAuth}>XXX</button>
+      <InfoPage />
+    </div>
+  );
 }
 
 function App() {
@@ -199,7 +248,7 @@ function App() {
   const [burgerIsOpen, setBurgerIsOpen] = useState(false);
   const [infoIsOpen, setInfoIsOpen] = useState(false);
   return (
-    <div style={{ height: "100vh", display: "flex", flexDirection: "column" }}>
+    <div style={{ height: "100vh", display: "flex", flexDirection: "column", backgroundColor: "E1D6E7" }}>
       {page === "landing" ? (
         <LandingPage goToAuth={() => setPage("auth")} />
       ) : (
@@ -208,10 +257,11 @@ function App() {
             style={{
               display: "flex",
               padding: "20px",
-              borderBottom: "1px solid black",
+              borderBottom: "0px solid black",
+              background: page === "autism" ? "EFEEEC" : "EFEEEC",
             }}
           >
-            <span>Контур системы</span>
+            <span>РљРѕРЅС‚СѓСЂ СЃРёСЃС‚РµРјС‹</span>
             <Burger
               isOpen={burgerIsOpen}
               open={() => setBurgerIsOpen(true)}
@@ -228,7 +278,7 @@ function App() {
                   }}
                   style={{ display: "block", marginTop: "10px" }}
                 >
-                  Лендинг
+                  Р›РµРЅРґРёРЅРі
                 </button>
                 <button
                   onClick={() => {
@@ -237,27 +287,27 @@ function App() {
                   }}
                   style={{ display: "block", marginTop: "10px" }}
                 >
-                  Авторизация
+                  РђРІС‚РѕСЂРёР·Р°С†РёСЏ
                 </button>
               </div>
             ) : page === "auth" ? (
-              <AuthPage />
-              // ) : page === "burger" ? (
+              <AuthPage goToAutismPage={() => setPage("autism")} />
+            ) : // ) : page === "burger" ? (
               //   <BurgerPage />
               // ) : page === "info" ? (
               //   <InfoPage />
-            )
-              : infoIsOpen ? (
-                <div style={{ border: "1px solid black", width: "100%" }}><button
-                  onClick={() => {
-                    setPage("landing");
-                    setInfoIsOpen(false);
-                  }}
-                  style={{ display: "block", marginTop: "10px" }}
-                >
-                  1234567
-                  {/* <h1>Информационно учебный полигон Катариум</h1>
-                  <p>О системе</p>
+              infoIsOpen ? (
+                <div style={{ border: "1px solid black", width: "100%" }}>
+                  <button
+                    onClick={() => {
+                      setPage("landing");
+                      setInfoIsOpen(false);
+                    }}
+                    style={{ display: "block", marginTop: "10px" }}
+                  >
+                    1234567
+                    {/* <h1>РРЅС„РѕСЂРјР°С†РёРѕРЅРЅРѕ СѓС‡РµР±РЅС‹Р№ РїРѕР»РёРіРѕРЅ РљР°С‚Р°СЂРёСѓРј</h1>
+                  <p>Рћ СЃРёСЃС‚РµРјРµ</p>
                   <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem ipsum
                     v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
                     v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
@@ -265,16 +315,16 @@ function App() {
                     v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
                     v Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum v v v v vLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
                   </p> */}
-                </button>
+                  </button>
                 </div>
               ) : page === "auth" ? (
                 <InfoPage />
-                // ) : page === "burger" ? (
-                //   <BurgerPage />
+              ) : page === "autism" ? (
+                <AutismPage goToAuth={() => setPage("auth")} /> // ) : page === "burger" ? (
+              ) : //   <BurgerPage />
                 // ) : page === "info" ? (
                 //   <InfoPage />
-              )
-                : null}
+                null}
           </main>
         </>
       )}
